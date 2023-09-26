49d9fc554d7b862e914e0c743b680e21b676de24
vsg16 <vsg16@tpu.ru>
2023-09-26 22:45:52 +0700

    last_time_commit

--- 2LB.py
+++ 2LB.py
@@ -3,7 +3,6 @@ import class1_
 import sys
 
 available_operation = ["Чтение", "Запись", "Модификация", "Передача прав"]
-Objects = 3
 Objects_files = ["CD-RW", "My_notes.txt", "Secret_file.csv"]
 Users = ["Пользователь123", "Босс", "Владислав", "Станислав", "Мухаммад"]
 safety_politics = class1_.relation_safety_politics(available_operation, Objects_files, Users)
